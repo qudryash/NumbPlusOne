fcf9ed6768a7fe0687c17c5594d5fe29326dfb45
qudryash <qudryashovav@yandex.ru>
2023-10-16 18:49:16 +0700

    Первая версия приложения-счётчик:
    Базовая задача
    
    В приложении реализована одна большая кнопка. Сколько раз на неё нажали — такая цифра и отображается на счётчике (т.е. при каждом нажатии на кнопку это число должно увеличиваться на 1).
    По нажатию на кнопку значение счётчика меняется — и текст в лейбле тоже.
    При запуске приложения у label значение равно: "Значение счетчика: 0"

--- NumbPlusOne/ViewController.swift
+++ NumbPlusOne/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numbValue = 0                               // Объявляем переменную numbValue со значением 0
+    
+    @IBOutlet weak var numb: UILabel!               // Добавление привязки лейбла к коду
+    @IBOutlet weak var numbPlusOneText: UIButton!   // Добавление привязки кнопки к коду
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //        При запуске проекта загружаем входные данные:
+                
+                numb.text = "Значение счётчика: \(numbValue)"   // Изменение названия text на "Значение счётчика: 0"
+                numbPlusOneText.setTitle("+1", for: .normal)    // Изменение название title на "+1"
+
     }
 
-
+    //    Добавляем действие для кнопки (при нажатии на кнопку обновляем переменную numbValue на значение +1)
+    @IBAction func numbPlusOne(_ sender: Any) {
+        
+        numbValue += 1
+        numb.text = "Значение счётчика: \(numbValue)"
+        
+    }
+    
 }
 
